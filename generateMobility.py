88ba1305346a81448b1611b03f4b4919069154bb
Tanguy Ropitault <tanguy.ropitault@nist.gov>
2024-03-08 10:27:59 -0500

    Cleaning

--- generateMobility.py
+++ generateMobility.py
@@ -1,12 +1,42 @@
+
+#  NIST-developed software is provided by NIST as a public service. You may use,
+#  copy and distribute copies of the software in any medium, provided that you
+#  keep intact this entire notice. You may improve,modify and create derivative
+#  works of the software or any portion of the software, and you may copy and
+#  distribute such modifications or works. Modified works should carry a notice
+#  stating that you changed the software and should note the date and nature of
+#  any such change. Please explicitly acknowledge the National Institute of
+#  Standards and Technology as the source of the software.
+ 
+#  NIST-developed software is expressly provided "AS IS." NIST MAKES NO
+#  WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY OPERATION OF
+#  LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
+#  AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR WARRANTS THAT THE
+#  OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED OR ERROR-FREE, OR THAT
+#  ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY
+#  REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR THE RESULTS THEREOF,
+#  INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY, RELIABILITY,
+#  OR USEFULNESS OF THE SOFTWARE.
+ 
+#  You are solely responsible for determining the appropriateness of using and
+#  distributing the software and you assume all risks associated with its use,
+#  including but not limited to the risks and costs of program errors,
+#  compliance with applicable laws, damage to or loss of data, programs or
+#  equipment, and the unavailability or interruption of operation. This
+#  software is not intended to be used in any situation where a failure could
+#  cause risk of injury or damage to property. The software developed by NIST
+#  employees is not subject to copyright protection within the United States.
+ 
+
+
 import random
 import numpy as np
-import matplotlib.pyplot as plt
 import math
-import time
 import csv
 import os
 import json
-import shutil  # Add this import at the beginning of your script
+import shutil
 
 
 
@@ -114,7 +144,7 @@ for idScenario in range(numberOfScenarioToGenerate):
         yaw, pitch, roll = calculate_rotation(patientLimb, [x, y, z])
         sta = MovingSTA(x, y, z, yaw, pitch, roll, ap)
 
-        # add STA to list of moving STAs
+        # Add STA to list of moving STAs
         sta_itself = []
         sta_itself.append(sta)
         moving_stas.append(sta_itself)
@@ -216,7 +246,6 @@ for idScenario in range(numberOfScenarioToGenerate):
 
             with open(fileInput, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=',')
-                # writer.writerow(['x', 'y'])
                 for point in coord:
                     writer.writerow(point)
 
@@ -224,7 +253,6 @@ for idScenario in range(numberOfScenarioToGenerate):
                 scenarioFolder, textFolder, "NodeRotation"+str(idNode-1)+".dat")
             with open(fileInput, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile, delimiter=',')
-                # writer.writerow(['x', 'y'])
                 for point in rot:
                     writer.writerow(point)
 
